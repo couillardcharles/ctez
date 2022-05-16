ccbe18c1380b7f22cc5296a2fa87e6ec0cec55a3
Charles Couillard <charles@benderlabs.io>
2022-05-16 11:27:45 +0200

    temp: ugly ithaca testnet support

--- deploy.sh
+++ deploy.sh
@@ -4,38 +4,36 @@ set -x
 mkdir _build
 
 # Modify as need to deploy on different networks
-TZC="../Tezos/tezos/tezos-client --mode mockup --protocol PtEdo2ZkT9oKpimTah6x2embF25oss54njMuPzkJTEi5RqfdZFA --base-dir _build/mockup"
+LIGO="docker run --rm -v "${PWD}":"${PWD}" -w "${PWD}" ligolang/ligo:0.42.0"
+TZC="tezos-client -E https://ithacanet.ecadinfra.com/"
 
-rm -rf _build/mockup
-$TZC create mockup
-
-deployment_key="bootstrap1"
-deployment_key_address=`$TZC show address ${deployment_key} | head -n 1 | awk '{print $2}'`
+deployment_key="alice"
+deployment_key_address=`tz1THJRdgsBA11ffM76p4PQm7jxHuwMZ8vQy`
 
 DEPLOYMENT_DATE=$(date '+%Y-%m-%d')
 
 # Build and deploy ctez
-ligo compile-contract ctez.mligo main > _build/ctez.tz
-ligo compile-storage ctez.mligo main "$(sed s/DEPLOYMENT_DATE/${DEPLOYMENT_DATE}/ < ctez_initial_storage.mligo)" > _build/ctez_storage.tz
+$LIGO compile contract ctez.mligo -e main -p ithaca > _build/ctez.tz
+$LIGO compile storage ctez.mligo -e main -p ithaca "$(sed s/DEPLOYMENT_DATE/${DEPLOYMENT_DATE}/ < ctez_initial_storage.mligo)" > _build/ctez_storage.tz
 $TZC originate contract ctez transferring 0 from $deployment_key running 'file:_build/ctez.tz' --init "$(<_build/ctez_storage.tz)" --burn-cap 10
 CTEZ_ADDRESS=`$TZC show known contract ctez`
 
 # Build and deploy the fa12 for ctez
-ligo compile-contract fa12.mligo main > _build/fa12.tz
-ligo compile-storage fa12.mligo main "$(sed s/ADMIN_ADDRESS/$CTEZ_ADDRESS/ < fa12_ctez_initial_storage.mligo)" > _build/fa12_ctez_storage.tz
+$LIGO compile contract fa12.mligo -e main -p ithaca > _build/fa12.tz
+$LIGO compile storage fa12.mligo -e main -p ithaca "$(sed s/ADMIN_ADDRESS/$CTEZ_ADDRESS/ < fa12_ctez_initial_storage.mligo)" > _build/fa12_ctez_storage.tz
 $TZC originate contract fa12_ctez transferring 0 from $deployment_key running 'file:_build/fa12.tz' --init "$(<_build/fa12_ctez_storage.tz)" --burn-cap 10
 FA12_CTEZ_ADDRESS=`$TZC show known contract fa12_ctez`
 
 # Build and deploy cfmm
-ligo compile-contract cfmm_tez_ctez.mligo main > _build/cfmm.tz
+$LIGO compile contract cfmm_tez_ctez.mligo -e main -p ithaca > _build/cfmm.tz
 sed s/FA12_CTEZ/${FA12_CTEZ_ADDRESS}/ < cfmm_initial_storage.mligo | sed s/CTEZ_ADDRESS/${CTEZ_ADDRESS}/ > _build/cfmm_storage.mligo
 
-ligo compile-storage cfmm_tez_ctez.mligo main "$(<_build/cfmm_storage.mligo)" > _build/cfmm_storage.tz
+$LIGO compile storage cfmm_tez_ctez.mligo -e main -p ithaca "$(<_build/cfmm_storage.mligo)" > _build/cfmm_storage.tz
 $TZC originate contract cfmm transferring 0.000001 from $deployment_key running 'file:_build/cfmm.tz' --init "$(<_build/cfmm_storage.tz)" --burn-cap 10
 CFMM_ADDRESS=`$TZC show known contract cfmm`
 
 # Build and deploy the fa12 for the cfmm lqt, specifying the cfmm as admin
-ligo compile-storage fa12.mligo main "$(sed s/ADMIN_ADDRESS/$CFMM_ADDRESS/ < fa12_ctez_initial_storage.mligo)" > _build/fa12_lqt_storage.tz
+$LIGO compile storage fa12.mligo -e main -p ithaca "$(sed s/ADMIN_ADDRESS/$CFMM_ADDRESS/ < fa12_ctez_initial_storage.mligo)" > _build/fa12_lqt_storage.tz
 $TZC originate contract fa12_lqt transferring 0 from $deployment_key running 'file:_build/fa12.tz' --init "$(<_build/fa12_lqt_storage.tz)" --burn-cap 10
 FA12_LQT_ADDRESS=`$TZC show known contract fa12_lqt`
 
